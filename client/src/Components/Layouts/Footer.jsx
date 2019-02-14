a2e77a68ff262384282680bf78c7c2103143c656
baklavacado <benny.l.wu@gmail.com>
2019-02-14 14:15:44 -0800

    add focus and selection function

--- client/src/Components/Layouts/Footer.jsx
+++ client/src/Components/Layouts/Footer.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { Paper, Tabs } from '@material-ui/core';
 import { Tab } from '@material-ui/core';
 
-export default ({categories, onSelect, focus}) => {
+export default ({categories, focus, onSelect}) => {
   const index = focus
     ? categories.findIndex(group => group === focus) + 1
-    : 0
+    : 0;
+
+  const onIndexSelect = (e, index) => 
+    onSelect(index === 0 ? '' : categories[index -1 ]);
 
   return <Paper>
     <Tabs
       value={index}
+      onChange = {onIndexSelect}
       indicatorColor="primary"
       textColor="primary"
       centered
