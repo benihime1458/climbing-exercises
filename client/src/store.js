89599cd4c66f771dcae35f1ba607553d8017b097
baklavacado <benny.l.wu@gmail.com>
2019-02-20 11:48:50 -0800

    delete duplicate store js

--- client/src/store.js
+++ client/src/store.js
@@ -1,11 +1,26 @@
+// FINAL CATEGORIES NOT BASED ON JUST WARMUPS
+// const categories = [
+//   'warm ups',
+//   'climbing',
+//   'training',
+//   'conditioning',
+// ];
+
+// ONLY WARMUPS AND ITS GROUP PROPERTIES AS STAND IN CATEGORIES
+// REVERT BACK TO FINAL CATEGORIES EVENTUALLY. 
+//CURRENTLY USED FOR TESTING REACT COMPONENTS AND STATES
 const categories = [
-  'cardio',
-  'dynamic stretching',
-  'on the wall'
+  'machines',
+  'bodyweight',
+  'on the wall',
+  'rotation',
+  'arms',
+  'wrists and fingers',
+  'wall warm ups'
 ];
 
-const cardio = [
-  {
+const warmups = {
+  'CARDIO': [{
     id: 'row',
     title: 'Row',
     description: 'Rowing Exercises...',
@@ -52,11 +67,8 @@ const cardio = [
     title: 'Lunges',
     description: 'Lunges...',
     group: 'bodyweight'
-  },
-];
-
-const stretches = [
-  {
+  }],
+  'STRETCH': [{
     id: 'flags',
     title: 'Flags',
     description: 'Flagging...',
@@ -129,40 +141,66 @@ const stretches = [
     group: 'arms'
   },
   {
-    id: 'wrist tendon glides', 
+    id: 'wrist tendon glides',
     title: 'Wrist Tendon Glides',
     description: 'Wrist Tendon Glides',
     group: 'wrists and fingers'
   },
   {
-    id: 'finger tendon glides', 
+    id: 'finger tendon glides',
     title: 'Finger Tendon Glides',
     description: 'Finger Tendon Glides',
     group: 'wrists and fingers'
-  }
-];
-
-const wall = [
-  {
+  }],
+  'WALL': [{
     id: 'traverse',
     title: 'Traverse',
     description: 'Traverse Exercises...',
+    group: 'wall warm ups'
   },
   {
     id: 'pyramid',
     title: 'Easy Pyramid',
     description: 'Pyramid...',
+    group: 'wall warm ups'
+
   },
   {
     id: 'sloth',
     title: 'Sloth Climbing',
     description: 'Sloth Climbing...',
+    group: 'wall warm ups'
+
   },
   {
     id: 'monkey',
     title: 'Monkey Climbing',
     description: 'Monkey Climbing',
-  },
-];
+    group: 'wall warm ups'
+  }]
+};
 
-export {categories, cardio, stretches, wall};
+const training = {
+  'CORE': [],
+  'FOREARMS': [],
+  'BICEPS': [],
+  'BACK': [],
+  'LEGS': [],
+};
+
+const climbing = {
+  'POWER': [],
+  'ENDURANCE': [],
+  'POWER ENDURANCE': [],
+  'TECHNIQUE': [],
+  'RECOVERY': [],
+};
+
+const conditioning = {
+  'ANTAGONIST': [],
+  'ENDURANCE': [],
+  'FLEXIBILITY': [],
+  'CARDIO': []
+};
+
+export { categories, warmups, training, climbing, conditioning };
